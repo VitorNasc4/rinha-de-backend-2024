76a2b49fef9287c516db979b101a39308aad56e8
vitor <vitornascimento@321@hotmail.com>
2024-02-20 20:02:32 -0300

    wip

--- api/Program.cs
+++ api/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// ApplyMigrations(app);
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -36,7 +35,7 @@ app.MapPost("/clientes/{id}/transacoes", async (int id, CreateTranscaoDTO transa
 {
   if (!transacaoModel.IsValid())
   {
-    return Results.BadRequest("Transação inválida.");
+    return Results.UnprocessableEntity();
   }
 
   if (!clientes.ContainsKey(id))
@@ -44,17 +43,37 @@ app.MapPost("/clientes/{id}/transacoes", async (int id, CreateTranscaoDTO transa
     return Results.NotFound("Cliente não encontrado.");
   }
 
-  var limite = clientes[id];
-  var transacao = mapper.Map<Transacao>(transacaoModel);
-  transacao.IdCliente = id;
-
-  await using var requisicao = await context.Database.BeginTransactionAsync(IsolationLevel.Serializable);
-  var cliente = await context.Clientes!.FirstAsync(c => c.Id == id);
-
-  if (transacao.Tipo == "c")
+  try
   {
-    cliente.Saldo += transacao.Valor;
+    var limite = clientes[id];
+    var transacao = mapper.Map<Transacao>(transacaoModel);
+    transacao.IdCliente = id;
+
+    await using var requisicao = await context.Database.BeginTransactionAsync(IsolationLevel.Serializable);
+    var cliente = await context.Clientes!.FirstAsync(c => c.Id == id);
+
+    if (transacao.Tipo == "c")
+    {
+      cliente.Saldo += transacao.Valor;
+      context.Transacoes!.Add(transacao);
+      await context.SaveChangesAsync();
+      await requisicao.CommitAsync();
+
+      return Results.Ok(new ReturnTransacoesDTO
+      {
+        Saldo = cliente.Saldo,
+        Limite = limite
+      });
+    }
+
+    if (cliente.Saldo - transacao.Valor < -limite)
+    {
+      return Results.UnprocessableEntity();
+    }
+
+    cliente.Saldo -= transacao.Valor;
     context.Transacoes!.Add(transacao);
+
     await context.SaveChangesAsync();
     await requisicao.CommitAsync();
 
@@ -64,28 +83,10 @@ app.MapPost("/clientes/{id}/transacoes", async (int id, CreateTranscaoDTO transa
       Limite = limite
     });
   }
-
-  if (cliente.Saldo - transacao.Valor < -limite)
+  catch (Exception e)
   {
-    return Results.Problem(new ProblemDetails
-    {
-      Status = 422,
-      Title = "Não é possível completar a transação",
-      Detail = "A transação excede o limite de saldo disponível do cliente."
-    });
+    return Results.BadRequest(e.Message);
   }
-
-  cliente.Saldo -= transacao.Valor;
-  context.Transacoes!.Add(transacao);
-
-  await context.SaveChangesAsync();
-  await requisicao.CommitAsync();
-
-  return Results.Ok(new ReturnTransacoesDTO
-  {
-    Saldo = cliente.Saldo,
-    Limite = limite
-  });
 });
 
 app.MapGet("/clientes/{id}/extrato", async (int id, Context context) =>
@@ -94,58 +95,47 @@ app.MapGet("/clientes/{id}/extrato", async (int id, Context context) =>
   {
     return Results.NotFound("Cliente não encontrado.");
   }
-  var limite = clientes[id];
-
-  var extrato = await context.Clientes!
-    .Where(cli => cli.Id == id)
-    .Select(cli => new
-    {
-      Saldo = cli.Saldo,
-      Limite = limite,
-      Data_Extrato = DateTime.Now,
-      Ultimas_Transacoes = context.Transacoes!
-        .Where(t => t.IdCliente == id)
-        .OrderByDescending(t => t.CreatedAt)
-        .Take(10)
-        .Select(t => new ReadTransacoesDTO
-        {
-          Valor = t.Valor,
-          Tipo = t.Tipo,
-          Descricao = t.Descricao,
-          Realizada_Em = t.CreatedAt
-        }).ToList()
-    })
-    .FirstOrDefaultAsync();
-
-  var retornoExtrato = new returnExtratoDTO()
+  try
   {
-    Saldo = new ReadSaldoDTO()
-    {
-      Total = extrato!.Saldo,
-      Limite = extrato.Limite,
-      Data_Extrato = extrato.Data_Extrato
-    },
-    Ultimas_Transacoes = extrato.Ultimas_Transacoes
-  };
+    var limite = clientes[id];
+    var extrato = await context.Clientes!
+      .Where(cli => cli.Id == id)
+      .Select(cli => new
+      {
+        Saldo = cli.Saldo,
+        Limite = limite,
+        Data_Extrato = DateTime.Now,
+        Ultimas_Transacoes = context.Transacoes!
+          .Where(t => t.IdCliente == id)
+          .OrderByDescending(t => t.CreatedAt)
+          .Take(10)
+          .Select(t => new ReadTransacoesDTO
+          {
+            Valor = t.Valor,
+            Tipo = t.Tipo,
+            Descricao = t.Descricao,
+            Realizada_Em = t.CreatedAt
+          }).ToList()
+      })
+      .FirstOrDefaultAsync();
 
-  return Results.Ok(retornoExtrato);
+    var retornoExtrato = new returnExtratoDTO()
+    {
+      Saldo = new ReadSaldoDTO()
+      {
+        Total = extrato!.Saldo,
+        Limite = extrato.Limite,
+        Data_Extrato = extrato.Data_Extrato
+      },
+      Ultimas_Transacoes = extrato.Ultimas_Transacoes
+    };
+
+    return Results.Ok(retornoExtrato);
+  }
+  catch (Exception e)
+  {
+    return Results.BadRequest(e.Message);
+  }
 });
 
 app.Run();
-
-// void ApplyMigrations(IApplicationBuilder app)
-// {
-//   using (var scope = app.ApplicationServices.CreateScope())
-//   {
-//     try
-//     {
-//       var dbContext = scope.ServiceProvider.GetRequiredService<Context>();
-//       dbContext.Database.Migrate();
-//     }
-//     catch (Exception ex)
-//     {
-//       var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-//       logger.LogError(ex, "Ocorreu um erro ao aplicar as Migrations");
-//     }
-//   }
-// }
